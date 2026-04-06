c79b3fa6bbb1a20263d7d4b640bfc0665b2434f6
vzdfdzc-pixel <vzdfdzc@gmail.com>
2026-04-05 22:46:26 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 using namespace geode::prelude;
 
@@ -7,7 +8,6 @@ class VaultCodesPopup : public geode::Popup<> {
 protected:
     bool setup() override {
         setTitle("Códigos de Vaults");
-
         auto winSize = m_mainLayer->getContentSize();
 
         struct VaultEntry {
@@ -31,12 +31,12 @@ protected:
             }
         };
 
-        auto scroll = ScrollLayer::create({ winSize.width - 20, winSize.height - 60 });
-        scroll->setPosition({ 10, 10 });
-
         float totalH = 0;
         for (auto& v : vaults)
             totalH += 25 + v.codes.size() * 18 + 8;
+
+        auto scroll = ScrollLayer::create({ winSize.width - 20, winSize.height - 60 });
+        scroll->setPosition({ 10, 10 });
 
         float yPos = totalH;
         auto content = CCLayer::create();
@@ -64,65 +64,18 @@ protected:
         scroll->m_contentLayer->addChild(content);
         scroll->moveToTop();
         m_mainLayer->addChild(scroll);
-
         return true;
     }
 
 public:
     static VaultCodesPopup* create() {
         auto ret = new VaultCodesPopup();
-        if (ret->init(320, 240, "GJ_square01.png")) {
+        if (ret->initAnchored(320, 240)) {
             ret->autorelease();
             return ret;
         }
         delete ret;
         return nullptr;
-    }
-};
-
-class DraggableButton : public CCSprite {
-    bool m_dragging = false;
-    CCPoint m_offset;
-
-public:
-    static DraggableButton* create() {
-        auto ret = new DraggableButton();
-        if (ret->initWithSpriteFrameName("GJ_lock_open_001.png")) {
-            ret->autorelease();
-            ret->setScale(1.1f);
-            ret->setOpacity(200);
-            auto ws = CCDirector::get()->getWinSize();
-            ret->setPosition({ ws.width - 25, ws.height / 2 });
-            return ret;
-        }
-        delete ret;
-        return nullptr;
-    }
-
-    bool ccTouchBegan(CCTouch* touch, CCEvent*) override {
-        auto loc = touch->getLocation();
-        if (boundingBox().containsPoint(loc)) {
-            m_dragging = true;
-            m_offset = getPosition() - loc;
-            return true;
-        }
-        return false;
-    }
-
-    void ccTouchMoved(CCTouch* touch, CCEvent*) override {
-        if (m_dragging)
-            setPosition(touch->getLocation() + m_offset);
-    }
-
-    void ccTouchEnded(CCTouch* touch, CCEvent*) override {
-        if (m_dragging) {
-            m_dragging = false;
-            float dist = ccpDistance(touch->getLocation(),
-                                     touch->getStartLocation());
-            if (dist < 10) {
-                VaultCodesPopup::create()->show();
-            }
-        }
     }
 };
 
@@ -130,14 +83,25 @@ class $modify(MenuLayer) {
     bool init() override {
         if (!MenuLayer::init()) return false;
 
-        auto btn = DraggableButton::create();
-        btn->setTag(9999);
-        addChild(btn, 100);
+        auto ws = CCDirector::get()->getWinSize();
 
-        CCDirector::get()->getTouchDispatcher()->addTargetedDelegate(
-            btn, -500, true
+        auto spr = CCSprite::createWithSpriteFrameName("GJ_lock_open_001.png");
+        spr->setScale(1.1f);
+
+        auto btn = CCMenuItemSpriteExtra::create(
+            spr, this,
+            menu_selector(MenuLayerExt::onVaultCodes)
         );
 
+        auto menu = CCMenu::create();
+        menu->addChild(btn);
+        menu->setPosition({ ws.width - 25, ws.height / 2 });
+        addChild(menu, 100);
+
         return true;
+    }
+
+    void onVaultCodes(CCObject*) {
+        VaultCodesPopup::create()->show();
     }
 };
